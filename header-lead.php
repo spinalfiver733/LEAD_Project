da2e8cc5ff0df6c8141f40ac9a763c081dcd8da8
Luis <spinalfiver733@gmail.com>
2025-01-15 00:24:15 -0600

    Cambios en el segundo header

--- header-lead.php
+++ header-lead.php
@@ -1,4 +1,4 @@
 <div class="lead-header">
-    <h2>Leadership For Environment And Development (LEAD)</h2>
+    <h1>Leadership For Environment And Development (LEAD)</h1>
     <p><strong>is a global network of people and organizations committed to sustainable development</strong></p>
 </div>
