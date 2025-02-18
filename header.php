b38d00b0d4f316ec646cc01c6d0f365532c2727a
Luis Vazquez <spinalfiver733@gmail.com>
2025-02-18 12:26:53 -0600

    Cambios en el header

--- header.php
+++ header.php
@@ -1,39 +1,29 @@
-<!-- header.php -->
 <header>
-    <div class="top-nav">
-        <ul>
-            <li><a href="index.php">Inicio</a></li>
-            <li>APLICAR</li>
-            <li>VISITAR</li>
-            <li>ESTUDIANTES</li>
-            <li>PROFESORADO</li>
-            <li>EXALUMNOS</li>
-            <li>
-                <div class="search-container">
-                    <input type="search" placeholder="Buscar...">
-                    <img src="assets/busqueda-de-lupa.svg" alt="buscar" class="search-icon">
-                </div>
-            </li>
-        </ul>
-    </div>
-    
     <div class="main-header">
         <a href="index.php">
-            <img class="main-header texto-lead" src="assets/TextoLEAD.webp" alt="LEAD logo">
+            <img src="assets/WEBP/LEAD México logo.webp" alt="LEAD logo">
         </a>
-        <img class="main-header" src="assets/LogoLEAD.webp" alt="LEAD logo">
+        <div class="divisor">
+            <div class="search-container">
+                <input type="search" placeholder="Buscar...">
+                <img src="assets/busqueda-de-lupa.svg" alt="buscar" class="search-icon">
+            </div>
+            <div class="texto-lead">
+                <h1>Inspirando  liderazgos  para  un  mundo  sustentable</h1>
+            </div>
+        </div>
     </div>
-  
+
     <nav class="main-nav">
         <ul>
             <li><a href="whosLEAD.php"><strong>¿Qué es Lead?</strong></a></li>
             <li><a href="consejoDirectivo.php"><strong>Consejo Directivo</strong></a></li>
             <li><a href="staffLEAD.php"><strong>Staff</strong></a></li>
-            <li><a href="publicaciones.php"><strong>Publicaciones</strong></li></a>
+            <li><a href="publicaciones.php"><strong>Publicaciones</strong></a></li>
             <li><a href="mediateca.php"><strong>Mediateca</strong></a></li>
             <li><a href="asociados.php"><strong>Asociados</strong></a></li>
             <li>
-                <a href="assets\archivosPDF\SolicitudIngreso\ApplicationFormCohort16.doc.pdf" download style="text-decoration: none; cursor: pointer;">
+                <a href="assets\archivosPDF\SolicitudIngreso\ApplicationFormCohort16.doc.pdf" download>
                     <strong>Solicitud de ingreso</strong>
                 </a>
             </li>
